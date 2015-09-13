f967aa5592c2b0e572d88aef2a570cce9196fced
yrsegal <yrsegal@gmail.com>
2015-09-13 12:27:19 -0400

    decode now works

--- __init__.py
+++ __init__.py
@@ -5,16 +5,17 @@ path = os.path.dirname(__file__)
 emojis = json.load(open(os.path.join(path, "emojis.json")))
 chars = json.load(open(os.path.join(path, "mime.json")))
 
-emojisize = 2
 
 encode_dict = dict(zip(chars, emojis))
 decode_dict = dict(zip(emojis, chars))
 
 if sys.version_info.major == 2:
+	emojisize = 2
 	fastrange = xrange
 	requiredtype = str
 	charconvert = str
 else:
+	emojisize = 1
 	fastrange = range
 	def requiredtype(buf):
 		return bytes(buf, 'utf8')
@@ -28,7 +29,6 @@ def encode(buf):
 	return "".join(map(lambda x: encode_dict[charconvert(x)], b64buf))
 
 def decode(buf):
-	buf = requiredtype(buf)
-	splitbuf = [buf[i*emojisize:i*emojisize+emojisize] for i in fastrange(len(buf)/emojisize)]
-	b64buf = "".join(map(lambda x: decode_dict[charconvert(x)], splitbuf))
-	return base64.b64decode(b64buf)
+	splitbuf = [buf[i*emojisize:i*emojisize+emojisize] for i in fastrange(int(len(buf)/emojisize))]
+	b64buf = "".join(map(lambda x: decode_dict[x], splitbuf))
+	return base64.b64decode(requiredtype(b64buf)).decode()
